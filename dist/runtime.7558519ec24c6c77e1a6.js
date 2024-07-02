b6a7bba73b107b23dd6f0a085fe2b1464f02a13e
root <root@shabbirMachine.myguest.virtualbox.org>
2024-07-02 13:37:47 +0500

    this is the initial file

---
+++ dist/runtime.7558519ec24c6c77e1a6.js
@@ -0,0 +1,31 @@
+/*! @version pwa-studio: 0.0.1, @magento/venia-ui: ~11.3.0, @magento/upward-security-headers: ~1.0.14, @magento/pwa-theme-venia: ~2.4.0, @magento/pwa-buildpack: ~11.4.3, @magento/peregrine: ~14.2.1, @magento/pagebuilder: ~9.2.1, @magento/eslint-config: ~1.5.0, @magento/babel-preset-peregrine: ~1.2.3, @apollo/client: ~3.5.0 */
+!function(e){function webpackJsonpCallback(r){for(var _,f,t=r[0],d=r[1],n=r[2],o=0,u=[];o<t.length;o++)f=t[o],Object.prototype.hasOwnProperty.call(a,f)&&a[f]&&u.push(a[f][0]),a[f]=0
+for(_ in d)Object.prototype.hasOwnProperty.call(d,_)&&(e[_]=d[_])
+for(b&&b(r);u.length;)u.shift()()
+return c.push.apply(c,n||[]),checkDeferredModules()}function checkDeferredModules(){for(var e,r=0;r<c.length;r++){for(var _=c[r],f=!0,t=1;t<_.length;t++){var b=_[t]
+0!==a[b]&&(f=!1)}f&&(c.splice(r--,1),e=__webpack_require__(__webpack_require__.s=_[0]))}return e}var r={},a={19:0},c=[]
+function __webpack_require__(a){if(r[a])return r[a].exports
+var c=r[a]={i:a,l:!1,exports:{}},_=!0
+try{e[a].call(c.exports,c,c.exports,__webpack_require__),_=!1}finally{_&&delete r[a]}return c.l=!0,c.exports}__webpack_require__.e=function requireEnsure(e){var r=[],c=a[e]
+if(0!==c)if(c)r.push(c[2])
+else{var _=new Promise(function(r,_){c=a[e]=[r,_]})
+r.push(c[2]=_)
+var f,t=document.createElement("script")
+t.charset="utf-8",t.timeout=120,__webpack_require__.nc&&t.setAttribute("nonce",__webpack_require__.nc),t.src=function jsonpScriptSrc(e){return __webpack_require__.p+""+({5:"vendors~RootCmp_CATEGORY__default~RootCmp_SEARCH__default",6:"RootCmp_SEARCH__default",15:"RootCmp_CATEGORY__default",16:"RootCmp_CMS_PAGE__default",18:"i18n-en_US",20:"vendors~RootCmp_PRODUCT__default"}[e]||e)+"."+{1:"8e9ddcfc4f68c3144068",2:"c808c88f09ae4ed7b269",3:"5d4e872beaa035e05871",4:"4421de1ff0331d2024ad",5:"e4f46fb93a56bb1f16bc",6:"48dea05607b0fbf8b3ef",7:"28a1c5ca879154b054e7",8:"1fb4da4622f10a805c48",9:"6de468749515e6e8e8b3",10:"702bf3c873fd01ef456e",11:"76e0faa9baaef11a6e0b",12:"91ad69919b399e347a42",13:"1e4e50c6b3818874fb48",14:"89bd0f5f1cbd53e0b849",15:"737c5cdb66a95da0623c",16:"d0bf4d2a1e9c6edb3614",18:"5691932e4d92b5c68e64",20:"683a178bd178ff66d023",21:"54d5842b516d38c242b6",22:"91de7954773e4a87e17f",23:"e05f107c21e79b48ebd0",24:"f3f7f9b89caaf1102942",25:"3f5772cf24900ce9d539",26:"8ce3b4eb05a32cc57926",27:"5fd5bdb741a9ce05aa80",28:"bd1a5995c56b0377f9e5",29:"cf6ecd3da263f8a76527",30:"96d6f1923a08c63c24cf",31:"4a58232ffb9c14234a94",32:"4cff2a6025025eb1a6ac",33:"2364299bb4ee4165935c",34:"8e5111c4d26c4e1a9e10",35:"2684374c53c576ac9127",36:"3a03675e40de304f011e",37:"1ad5920f3dde3134a26e",38:"2d25cc7e1a94e1302b16",39:"94ce91bf51c806aff08e",40:"33004251b88bfd3c9ebb",41:"56f99cdcf08263211484",42:"bf390eaeae4d51dabfc8",43:"6358030d5336457ece7b",44:"cd3349a295a087bb9a8e",45:"9762f5618fcf2ac8e6aa",46:"9a7b3e7f4adbce367c53",47:"fc8ed4ddeb6cbbd36c7f",48:"a0e649cba5af3491d2ea",49:"8bf99f8bf046bdadd63b",50:"3d7c8914a864ec95ad0c",51:"db25fd147bb92c74ac1b",52:"fb380d997b9bbd42acaa",53:"c892de19954b14ff994e",54:"9dc7845d1da0cdd8af0c",55:"acfc398c1c10d5fb0d40",56:"8679f67693971cf6d265",57:"046898268f331032f205",58:"505379ad3bcb7a61b2d7",59:"626948823424a93e3f77"}[e]+".js"}(e)
+var b=new Error
+f=function(r){t.onerror=t.onload=null,clearTimeout(d)
+var c=a[e]
+if(0!==c){if(c){var _=r&&("load"===r.type?"missing":r.type),f=r&&r.target&&r.target.src
+b.message="Loading chunk "+e+" failed.\n("+_+": "+f+")",b.name="ChunkLoadError",b.type=_,b.request=f,c[1](b)}a[e]=void 0}}
+var d=setTimeout(function(){f({type:"timeout",target:t})},12e4)
+t.onerror=t.onload=f,document.head.appendChild(t)}return Promise.all(r)},__webpack_require__.m=e,__webpack_require__.c=r,__webpack_require__.d=function(e,r,a){__webpack_require__.o(e,r)||Object.defineProperty(e,r,{enumerable:!0,get:a})},__webpack_require__.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},__webpack_require__.t=function(e,r){if(1&r&&(e=__webpack_require__(e)),8&r)return e
+if(4&r&&"object"==typeof e&&e&&e.__esModule)return e
+var a=Object.create(null)
+if(__webpack_require__.r(a),Object.defineProperty(a,"default",{enumerable:!0,value:e}),2&r&&"string"!=typeof e)for(var c in e)__webpack_require__.d(a,c,function(r){return e[r]}.bind(null,c))
+return a},__webpack_require__.n=function(e){var r=e&&e.__esModule?function getDefault(){return e.default}:function getModuleExports(){return e}
+return __webpack_require__.d(r,"a",r),r},__webpack_require__.o=function(e,r){return Object.prototype.hasOwnProperty.call(e,r)},__webpack_require__.p="/",__webpack_require__.oe=function(e){throw e}
+var _=window.webpackJsonp=window.webpackJsonp||[],f=_.push.bind(_)
+_.push=webpackJsonpCallback,_=_.slice()
+for(var t=0;t<_.length;t++)webpackJsonpCallback(_[t])
+var b=f
+checkDeferredModules()}([])
